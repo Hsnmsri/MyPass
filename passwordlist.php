63906da7893e35ad1fec8dd055f574f8b4420cf0
Hossein Mansouri <hsnmsri82@gmail.com>
2022-08-05 16:00:21 +0430

    change pattern to .php

---
+++ passwordlist.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password List</title>
+</head>
+
+<body>
+
+    <h1>Password List</h1>
+    <button>Setting</button>
+    <button>Login History</button>
+    <button>Password List</button>
+    <button>Logout</button>
+
+    <br><br>
+    <br><br>
+
+    <table border="1">
+
+        <thead>
+            <tr>
+                <td>Domain</td>
+                <td>UserName</td>
+                <td>Password</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>mrmansouri.ir</td>
+                <td>hsnmsri</td>
+                <td>61542493</td>
+                <td><button>Edit</button></td>
+                <td><button>Delete</button></td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
---
+++ setting.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Setting</title>
+</head>
+
+<body>
+
+    <h1>Setting</h1>
+    <button>Setting</button>
+    <button>Login History</button>
+    <button>Password List</button>
+    <button>Logout</button>
+
+    <br><br>
+    <br><br>
+
+    <hr>
+    <div>
+        <h3>Change UserName</h3>
+        <label>UserName : </label>
+        <input type="text">
+        <br><br>
+        <label>New UserName : </label>
+        <input type="text">
+        <br><br>
+        <label>re-type UserName : </label>
+        <input type="text">
+        <br><br>
+        <button>Clear</button>
+        <button>Change UserName</button>
+        <br><br>
+    </div>
+    <hr>
+    <div>
+        <h3>Change Password</h3>
+        <label>Password : </label>
+        <input type="text">
+        <br><br>
+        <label>New Password : </label>
+        <input type="text">
+        <br><br>
+        <label>re-type Password : </label>
+        <input type="text">
+        <br><br>
+        <button>Clear</button>
+        <button>Change Password</button>
+        <br><br>
+    </div>
+
+</body>
+
+</html>
