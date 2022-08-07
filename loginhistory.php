b2f37a8c16d8cb06e9ab9d095188bf4c78bfbf51
Hossein Mansouri <hsnmsri82@gmail.com>
2022-08-08 03:30:58 +0430

    Complete login history

--- loginhistory.php
+++ loginhistory.php
@@ -11,30 +11,19 @@
 <body>
 
     <h1>Login History</h1>
+    <button onclick="Redirector('index.php')">Home</button>
     <button onclick="Redirector('setting.php')">Setting</button>
-    <button onclick="Redirector('loginhistory.php')">Login History</button>
     <button  onclick="Redirector('passwordlist.php')">Password List</button>
+    <button  onclick="Redirector('loginhistory.php')">Refresh</button>
     <button  onclick="Redirector('index.php?user=logout')">Logout</button>
 
     <br><br>
     <br><br>
 
-    <table border="1">
+    <?php include "./php/loginhistory.php" ?>
 
-        <thead>
-            <tr>
-                <td>Time</td>
-                <td>Date</td>
-                <td>IP</td>
-                <td>Detail</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
+    <!-- script -->
+    <script src="js/functions.js"></script>
 
 </body>
 
