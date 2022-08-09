4f021fa782c094e014f4c6cf051623b2ff6769f9
Hossein Mansouri <hsnmsri82@gmail.com>
2022-08-10 03:37:36 +0430

    change connection details

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,9 +1,9 @@
 <?php
 //Database information
-$host = 'localhost';
-$dbUserName = 'mypass_user';
-$dbPassword = 'hsnmsri61542493';
-$dbDataBaseName = 'mypass';
+$host = 'HOST_NAME';
+$dbUserName = 'USER_NAME';
+$dbPassword = 'PASSWORD';
+$dbDataBaseName = 'DATABASE_NAME';
 //Query
 $MyPassdbConnection = mysqli_connect($host, $dbUserName, $dbPassword, $dbDataBaseName);
 mysqli_query($MyPassdbConnection, "SET CHARACTER SET utf8");
