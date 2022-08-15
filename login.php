50b41c52714f34e26ffa073120a0512aabcada16
Pouya Rafieeyan <81816264+PouyaRafieeyan@users.noreply.github.com>
2022-08-15 23:13:02 +0430

    back ground image and main card

--- login.php
+++ login.php
@@ -6,19 +6,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+
+    <link rel="stylesheet" href="css/basestyle/basestyle.css">
+    <link rel="stylesheet" href="/css/login.css">
+
+    <title>Login | MyPass</title>
 </head>
 
 <body>
-    <form method="post">
-        <h1>Login</h1>
-        <label>UserName :</label>
+
+    <!-- back ground image -->
+    <img src="img/BG.png" alt="skybackground" class="back-ground">
+
+    <form method="post" class="main bgcolor-0D1321">
+
+        <h1 class="main-title color-fff">Login</h1>
+        
+        <div>
+        <label>UserName</label>
         <input type="text" name="username">
-        <br><br>
-        <label>Password :</label>
+        </div>
+
+        <div>
+        <label>Password</label>
         <input type="password" name="password">
-        <br><br>
+        </div>
+
         <button type="submit">Login</button>
+
     </form>
 </body>
 
