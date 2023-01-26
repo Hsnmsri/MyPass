0b69043a144f99618c33edb070c6ec61bd509be9
Hossein Mansouri <hsnmsri82@gmail.com>
2023-01-26 19:51:46 +0330

    Add js for setting page

---
+++ js/setting.js
@@ -0,0 +1,19 @@
+// Change Username frame
+$('#change-username-btn').click(function () {
+    $('#change-username-frame').show();
+    $('#setting-frame').hide();
+});
+$('#cancel-username-button').click(function () {
+    $('#change-username-frame').hide();
+    $('#setting-frame').show();
+});
+
+// Change Password frame
+$('#change-password-btn').click(function () {
+    $('#change-password-frame').show();
+    $('#setting-frame').hide();
+});
+$('#cancel-password-button').click(function () {
+    $('#change-password-frame').hide();
+    $('#setting-frame').show();
+});
